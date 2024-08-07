76c84fb9eaef740e26a55dda19aa4f56316e4bb0
eliote-geeks <pauleliote97@gmail.com>
2024-08-07 12:56:13 +0200

    update database

--- database/migrations/2024_08_06_170542_create_classe_cours_table.php
+++ database/migrations/2024_08_06_170542_create_classe_cours_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('school_information_id')->references('id')->on('school_informations')->onDelete('cascade');
             $table->timestamp('day');
             $table->foreignId('classe_id')->references('id')->on('classes')->onDelete('cascade');
-            $table->foreignId('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->time('start_Hour');
             $table->time('end_Hour');
             $table->boolean('status')->default(1);
