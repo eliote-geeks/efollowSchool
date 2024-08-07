6c4a8c83c86fa07950c604d40b7fe46c82d6d510
dylanalvin <dylanalvin28@gmail.com>
2024-08-07 16:03:12 +0100

    update preload image function

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -61,29 +61,18 @@
 
  </style>
 
-                <section class="container-fluid p-4">
+                 <section class="container-fluid p-4">
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-12">
                             <!-- Page header -->
                             <div class="border-bottom pb-3 mb-3">
                                 <div class="mb-2 mb-lg-0">
-                                    <h1 class="mb-0 h2 fw-bold">Add Customer</h1>
-                                    <!-- Breadcrumb -->
-                                    <nav aria-label="breadcrumb">
-                                        <ol class="breadcrumb">
-                                            <li class="breadcrumb-item">
-                                                <a href="admin-dashboard.html">Dashboard</a>
-                                            </li>
-                                            <li class="breadcrumb-item">
-                                                <a href="#">Ecommerce</a>
-                                            </li>
-                                            <li class="breadcrumb-item active" aria-current="page">Add Customer</li>
-                                        </ol>
-                                    </nav>
+                                    <h1 class="mb-0 h2 fw-bold">Créer un élève</h1>
                                 </div>
                             </div>
                         </div>
                     </div>
+
 
                     <div class="row">
                         <div class="offset-xl-2 col-xl-8 col-12">
@@ -92,7 +81,7 @@
                                 <div class="card mb-4">
                                     <!-- card body -->
                                     <div class="card-body">
-                                        <h2 class="mb-4 text-secondary">Créer un élève</h2>
+                                        <h2 class="mb-4 text-secondary">Remplir le formulaire</h2>
 
                                         <!-- row -->
                                         <div class="row gx-3">
@@ -229,5 +218,7 @@
                         </div>
                     </div>
                 </section>
+
+            
 
 </x-layouts>
